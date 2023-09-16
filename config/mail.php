03ec2a2c2f701d3ca0376d41696837a00b9eb847
mohamed elsaeed <mhmde6373@gmail.com>
2023-09-16 21:29:26 +0300

    16_9_23

--- config/mail.php
+++ config/mail.php
@@ -238,8 +238,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', '3mexam@gmail.com'),
+        'name' => env('MAIL_FROM_NAME', 'Antika'),
     ],
 
     /*
